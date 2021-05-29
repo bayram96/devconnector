1f10340e9d36375f0750c00a528d4d6826a20f93
Bayramgeldi <byrammr@gmail.com>
2021-05-29 05:13:13 -0400

    added update experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -202,4 +202,28 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience
+// @desc    Delete experience profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(res.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
