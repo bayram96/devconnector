2a0cf954e5181d202134cb46baf0f0a5fcd85970
Bayramgeldi <byrammr@gmail.com>
2021-06-02 22:09:06 -0400

    feat alert for login page

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
-export default function (state = initialState, action) {
+function alertReducer(state = initialState, action) {
   const { type, payload } = action;
+
   switch (type) {
     case SET_ALERT:
       return [...state, payload];
@@ -13,3 +15,5 @@ export default function (state = initialState, action) {
       return state;
   }
 }
+
+export default alertReducer;
