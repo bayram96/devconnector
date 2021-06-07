8c18855c9576336dd91a35dca84bc6d9ff958bd4
Bayramgeldi <byrammr@gmail.com>
2021-06-07 17:03:16 -0400

    feat login user

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,6 +4,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from "../actions/types";
 
 const initialState = {
@@ -25,10 +27,12 @@ function registerReducer(state = initialState, action) {
         user: payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return { ...state, token: null, isAuthenticated: true, loading: false };
 
